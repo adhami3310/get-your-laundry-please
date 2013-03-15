9878bc7103ee24e0fd75693210628ccbea448ffc
root <root@blueberry-crumpet.(none)>
2013-03-15 15:28:44 -0700

    Added support for multiple machines to arduino code

--- lib/EmonLib/examples/current_only/current_only.ino
+++ lib/EmonLib/examples/current_only/current_only.ino
@@ -1,7 +1,10 @@
 // EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
 
 #include "EmonLib.h"                   // Include Emon Library
+EnergyMonitor emon0;                   // Create an instance
 EnergyMonitor emon1;                   // Create an instance
+EnergyMonitor emon2;                   // Create an instance
+//EnergyMonitor emon3;                   // Create an instance
 
 float startTime;
 
@@ -9,15 +12,29 @@ void setup()
 {
 	Serial.begin(9600);
 
+	emon0.current(0, 111.1);             // Current: input pin, calibration.
 	emon1.current(1, 111.1);             // Current: input pin, calibration.
+	emon2.current(2, 111.1);             // Current: input pin, calibration.
+	//emon3.current(3, 111.1);             // Current: input pin, calibration.
 
 	startTime = millis();
 }
 
 void loop()
 {
-	double Irms = emon1.calcIrms(1480);  // Calculate Irms only
-  Serial.println(Irms);
+
+	double Irms0 = emon0.calcIrms(1480);  // Calculate Irms only
+	double Irms1 = emon1.calcIrms(1480);  // Calculate Irms only
+	double Irms2 = emon2.calcIrms(1480);  // Calculate Irms only
+	//double Irms3 = emon3.calcIrms(1480);  // Calculate Irms only
+  Serial.print(Irms0);
+  Serial.print(' ');
+  Serial.print(Irms1);
+  Serial.print(' ');
+  Serial.print(Irms2);
+  //Serial.print(' ');
+  //Serial.print(Irms3);
+  Serial.println();
 
   delay(100);
 
