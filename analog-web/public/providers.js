cd39bfc2197c8149d13b3b22caab2b56459451c7
Random Laundry Hackers <imperial-consorts@mit.edu>
2015-04-02 14:22:28 -0400

    Removed non-working cellular providers

--- analog-web/public/providers.js
+++ analog-web/public/providers.js
@@ -1,12 +1,7 @@
 var providers = [
-  { name: 'US Cellular', email: '%s@email.uscc.net' },
-  { name: 'Alltel', email: '%s@message.alltel.com' },
   { name: 'Sprint', email: '%s@messaging.sprintpcs.com' },
-  { name: 'Cellular One', email: '%s@mobile.celloneusa.com' },
   { name: 'Telus', email: '%s@msg.telus.com' },
   { name: 'Rogers AT&T Wireless', email: '%s@pcs.rogers.com' },
-  { name: 'Qwest', email: '%s@qwestmp.com' },
-  { name: 'T-Mobile', email: '%s@tmomail.net' },
   { name: 'AT&T', email: '%s@txt.att.net' },
   { name: 'Verizon', email: '%s@vtext.com' },
   { name: 'Virgin Mobile', email: '%s@vmobl.com' },
