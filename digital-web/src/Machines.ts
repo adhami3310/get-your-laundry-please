a949d75608ef43d23a16938e8a88b1c09d4f4dc5
Khaleel Al-Adhami <adhami@mit.edu>
2022-04-30 22:28:04 -0400

    added evaluation of data

--- digital-web/src/Machines.ts
+++ digital-web/src/Machines.ts
@@ -2,14 +2,22 @@ import { SerialPort } from 'serialport';
 import { ReadlineParser } from '@serialport/parser-readline';
 import assert from 'assert';
 
+const HISTORY_TIME = 60; //number of seonds to use to change data from ON to OFF
+const SHORT_TIME = 30; //number of seonds to use to change data from OFF to ON
+const DELAY = 2; // number of seconds between two incoming inputs from the arduino
+const LUDICROUS_CURRENT = 40; // above this the machine is acting weird and prob sth happened wrong
+const ON_THRESHOLD = 1; //the threshold to become ON
+const OFF_THRESHOLD = 1; //the threshold to become OFF
+
 export enum MachineStatus {
-    ON, OFF, BROKEN
+    ON, OFF, BROKEN, NOIDEA
 };
 
 export class Machines {
     private readonly serialPort: SerialPort;
     private buffer: string = "";
     private readonly status: Array<MachineStatus>;
+    private readonly history: Array<Array<number>> = [];
 
     public constructor(count: number, path: string, br: number) {
         this.serialPort = new SerialPort({ path: path, baudRate: br });
@@ -29,9 +37,10 @@ export class Machines {
     }
 
     public toString(): string {
-        return this.status.map((status)=>{
-            if(status === MachineStatus.ON) return "ON";
-            if(status === MachineStatus.OFF) return "OFF";
+        return this.status.map((status) => {
+            if (status === MachineStatus.ON) return "ON";
+            if (status === MachineStatus.OFF) return "OFF";
+            if (status === MachineStatus.NOIDEA) return "NOIDEA";
             return "BROKEN";
         }).join(" ");
     }
@@ -45,5 +54,47 @@ export class Machines {
         this.buffer = lines.slice(1).join("\n");
         console.log("what does the machine say?", firstLine);
         const values = firstLine.split(" ").map(val => parseFloat(val));
+        this.history.push(values);
+        for (let i = 0; i < this.status.length; i++) {
+            const currentStatus = this.status[i]!;
+            if(currentStatus === MachineStatus.BROKEN) continue;
+            const historyValues = [];
+            const shortValues = [];
+            for (let j = this.history.length - HISTORY_TIME / DELAY; j < this.history.length; j++) {
+                const value = this.history[j]![i];
+                if (value !== undefined && value != NaN && value <= LUDICROUS_CURRENT)
+                    historyValues.push(value);
+            }
+            for (let j = this.history.length - SHORT_TIME / DELAY; j < this.history.length; j++) {
+                const value = this.history[j]![i];
+                if (value !== undefined && value != NaN && value <= LUDICROUS_CURRENT)
+                    shortValues.push(value);
+            }
+            if (historyValues.length == 0 || shortValues.length == 0) {
+                this.changeStatus(i, MachineStatus.NOIDEA);
+                continue;
+            }
+            const historyAverage = historyValues.reduce((a, b) => a + b, 0) / historyValues.length;
+            const shortAverage = shortValues.reduce((a, b) => a + b, 0) / shortValues.length;
+            if(currentStatus === MachineStatus.NOIDEA) {
+                if(shortAverage >= ON_THRESHOLD) {
+                    this.changeStatus(i, MachineStatus.ON);
+                }else if(historyAverage <= OFF_THRESHOLD) {
+                    this.changeStatus(i, MachineStatus.OFF);
+                }
+            }else if(currentStatus === MachineStatus.OFF){
+                if(shortAverage >= ON_THRESHOLD){
+                    this.changeStatus(i, MachineStatus.ON);
+                }
+            }else if(currentStatus === MachineStatus.ON) {
+                if(shortAverage <= OFF_THRESHOLD && historyAverage <= OFF_THRESHOLD){
+                    this.changeStatus(i, MachineStatus.OFF);
+                }
+            }
+        }
+    }
+
+    private changeStatus(index: number, newStatus: MachineStatus): void {
+        this.status[index] = newStatus;
     }
 }
