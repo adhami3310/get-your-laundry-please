9f283bfd9105a8f0a8ba25f94db8d5a4b38ec515
Khaleel Al-Adhami <adhami@mit.edu>
2022-05-01 00:26:04 -0400

    added homepage fr

--- digital-web/dist/index.js
+++ digital-web/dist/index.js
@@ -6,10 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_status_codes_1 = __importDefault(require("http-status-codes"));
 const Machines_1 = require("./Machines");
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const washers = new Machines_1.Machines('Washers', 3, '/dev/ttyUSB1', 9600);
 const dryers = new Machines_1.Machines('Dryers', 4, '/dev/ttyUSB0', 9600);
-app.use(express_1.default.static(__dirname + '/public'));
+app.use('/', (requestion, response) => {
+    response.sendFile(path_1.default.join(__dirname, '/index.html'));
+});
 app.use('/watch', (request, response) => {
     response.status(http_status_codes_1.default.ACCEPTED).type("json").send({ "washers": washers.toJSON(), "dryers": dryers.toJSON() });
     // response.status(HttpStatus.ACCEPTED).type("text").send(`${washers.toString()}\n${dryers.toString()}\n`);
--- digital-web/src/index.ts
+++ digital-web/src/index.ts
@@ -3,13 +3,15 @@ import express, { Application } from 'express';
 import { Server } from 'http';
 import HttpStatus from 'http-status-codes';
 import { Machines } from './Machines';
-
+import path from 'path';
 
 const app = express();
 const washers = new Machines('Washers', 3, '/dev/ttyUSB1', 9600);
 const dryers = new Machines('Dryers', 4, '/dev/ttyUSB0', 9600);
 
-app.use(express.static(__dirname+'/public'));
+app.use('/', (requestion, response) => {
+    response.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.use('/watch', (request, response) => {
     response.status(HttpStatus.ACCEPTED).type("json").send({ "washers": washers.toJSON(), "dryers": dryers.toJSON() });
