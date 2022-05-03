ffae1f068ec0186a9c0fbf5ec1f8835a756d7f11
Khaleel Al-Adhami <adhami@mit.edu>
2022-05-03 00:17:34 -0400

    changed maachine order

--- digital-web/src/ForcedStates.ts
+++ digital-web/src/ForcedStates.ts
@@ -1,7 +1,7 @@
 import { MachineStatus } from "./Machines";
 
 export const forcedDryers = [MachineStatus.NONE, MachineStatus.NONE, MachineStatus.NONE, MachineStatus.NONE];
-export const dryersMapping = [0, 1, 2, 3];
+export const dryersMapping = [2, 1, 0, 3];
 
 export const forcedWashers = [MachineStatus.NONE, MachineStatus.NOIDEA, MachineStatus.NONE];
 export const washersMapping = [0, 1, 2];
