970b53c242ce48bfb923cfe6f8de6c94fc0317d9
Khaleel Al-Adhami <adhami@mit.edu>
2022-05-16 19:19:29 -0400

    removed empty values

--- digital-web/src/Machines.ts
+++ digital-web/src/Machines.ts
@@ -88,7 +88,7 @@ export class Machines {
     private onData(data: string): void {
         const receivedTime = Date.now();
         for(const line of data.split("\n")){
-            const values = line.split(" ").map(val => parseFloat(val));
+            const values = line.split(" ").filter(value => value.length !== 0).map(val => parseFloat(val));
             if(values.length === 0) continue;
             console.log(`${this.name}: [${values.join(", ")}]`);
             assert.strictEqual(values.length, this.count, "Expected number of values to match number of machines, check wiring.");
