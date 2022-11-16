86797c96fbd84d89722c0a9699d17f57584237ea
Adhami3310 <khaleel.aladhami@gmail.com>
2022-11-15 23:19:10 -0500

    removed forced states for debugging

--- digital-web/dist/ForcedStates.js
+++ digital-web/dist/ForcedStates.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.washersMapping = exports.forcedWashers = exports.dryersMapping = exports.forcedDryers = void 0;
 const Machines_1 = require("./Machines");
-exports.forcedDryers = [Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NOIDEA, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NOIDEA];
+exports.forcedDryers = [Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE];
 exports.dryersMapping = [2, 1, 3, 0];
-exports.forcedWashers = [Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NOIDEA, Machines_1.MachineStatus.NONE];
+exports.forcedWashers = [Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE];
 exports.washersMapping = [0, 1, 2];
 //# sourceMappingURL=ForcedStates.js.map
