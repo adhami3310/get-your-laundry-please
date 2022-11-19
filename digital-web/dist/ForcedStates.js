ba01cf07f8b139e6169e05c05a0cb8e315767b42
Adhami3310 <khaleel.aladhami@gmail.com>
2022-11-19 04:18:22 -0500

    changed mapping of dryers

--- digital-web/dist/ForcedStates.js
+++ digital-web/dist/ForcedStates.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.washersMapping = exports.forcedWashers = exports.dryersMapping = exports.forcedDryers = void 0;
 const Machines_1 = require("./Machines");
 exports.forcedDryers = [Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.BROKEN, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE];
-exports.dryersMapping = [2, 1, 3, 0];
+exports.dryersMapping = [2, 3, 1, 0];
 exports.forcedWashers = [Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE];
 exports.washersMapping = [0, 1, 2];
 //# sourceMappingURL=ForcedStates.js.map
