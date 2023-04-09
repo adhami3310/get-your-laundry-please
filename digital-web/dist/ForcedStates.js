a365700a0bf966261f8a3405c93e78f12e3f1802
Adhami3310 <khaleel.aladhami@gmail.com>
2023-04-09 08:04:30 -0400

    update broken again n

--- digital-web/dist/ForcedStates.js
+++ digital-web/dist/ForcedStates.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.washersDelay = exports.washersMapping = exports.forcedWashers = exports.dryersDelay = exports.dryersMapping = exports.forcedDryers = void 0;
 const Machines_1 = require("./Machines");
-exports.forcedDryers = [Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.BROKEN, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE];
+exports.forcedDryers = [Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.BROKEN];
 exports.dryersMapping = [2, 3, 1, 0];
 exports.dryersDelay = [0, 0, 0, 0];
 exports.forcedWashers = [Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.NONE, Machines_1.MachineStatus.BROKEN];
